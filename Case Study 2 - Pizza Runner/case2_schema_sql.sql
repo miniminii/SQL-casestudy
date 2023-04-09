b7752680cabb11e46a1c216de6149a436e66ee15
Yumin Kim <90626970+miniminii@users.noreply.github.com>
2023-04-09 22:30:15 +0900

    Update case2_schema_sql.sql

--- Case Study 2 - Pizza Runner/case2_schema_sql.sql
+++ Case Study 2 - Pizza Runner/case2_schema_sql.sql
@@ -1,5 +1,3 @@
-DROP SCHEMA pizza_runner;
-
 CREATE SCHEMA pizza_runner;
 USE pizza_runner;
 
@@ -116,4 +114,49 @@ VALUES
   (9, 'Peppers'),
   (10, 'Salami'),
   (11, 'Tomatoes'),
-  (12, 'Tomato Sauce');
+  (12, 'Tomato Sauce') ;
+
+-- Data Cleansing -> create temp table 
+DROP TABLE IF EXISTS customer_orders_temp;
+
+CREATE TABLE customer_orders_temp AS
+SELECT order_id,
+       customer_id,
+       pizza_id,
+       CASE
+           WHEN exclusions = '' THEN NULL
+           WHEN exclusions = 'null' THEN NULL
+           ELSE exclusions
+       END AS exclusions,
+       CASE
+           WHEN extras = '' THEN NULL
+           WHEN extras = 'null' THEN NULL
+           ELSE extras
+       END AS extras,
+       order_time
+FROM pizza_runner.customer_orders;
+
+DROP TABLE IF EXISTS runner_orders_temp;
+
+CREATE  TABLE runner_orders_temp AS
+
+SELECT order_id,
+       runner_id,
+       CASE
+           WHEN pickup_time LIKE 'null' THEN NULL
+           ELSE pickup_time
+       END AS pickup_time,
+       CASE
+           WHEN distance LIKE 'null' THEN NULL
+           ELSE CAST(regexp_replace(distance, '[a-z]+', '') AS FLOAT)
+       END AS distance,
+       CASE
+           WHEN duration LIKE 'null' THEN NULL
+           ELSE CAST(regexp_replace(duration, '[a-z]+', '') AS FLOAT)
+       END AS duration,
+       CASE
+           WHEN cancellation LIKE '' THEN NULL
+           WHEN cancellation LIKE 'null' THEN NULL
+           ELSE cancellation
+       END AS cancellation
+FROM pizza_runner.runner_orders;
